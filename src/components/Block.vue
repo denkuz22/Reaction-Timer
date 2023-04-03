822debe4b718550405621dd2fca53c301c886cf7
Denis Kuznetsov <denis.kuznetsovca1997@gmail.com>
2023-04-03 16:40:50 -0400

    added new functionlity to place the shape anywhere on the screen

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="block" v-if="showBlock" @click="stopTimer">click me</div>
+  <div class="block" v-if="showBlock" @click="stopTimer" :style="{ width: size + 'px', height: size + 'px', borderRadius: '50%', position: 'absolute', top: topPos + 'px', left: leftPos + 'px' }"></div>
 </template>
 
 <script>
@@ -9,10 +9,26 @@ export default {
     return {
       showBlock:false,
       timer: null,
-      reactionTime: null
+      reactionTime: null,
+      size:0,
+      topPos:0,
+      leftPos:0
     }
   },
   mounted() {
+          // Generate random values for topPos and leftPos
+    this.size = Math.floor(Math.random() * 100) + 200; // generate a random size between 50 and 150
+    this.size = Math.round(this.size / 2) * 2;
+    console.log(this.size)
+
+     // get the width and height of the parent element
+    const parentWidth = this.$el.parentElement.offsetWidth;
+    const parentHeight = this.$el.parentElement.offsetHeight;
+
+    // generate random positions for top and left properties
+    this.topPos = Math.floor(Math.random() * (parentHeight - this.size));
+    this.leftPos = Math.floor(Math.random() * (parentWidth - this.size));
+
     setTimeout(()=> {
       this.showBlock = true
       this.startTimer()
@@ -37,12 +53,13 @@ export default {
 
 <style>
   .block {
-    width: 400px;
-    border-radius: 20px;
+    /* width: 400px;
+    height: 300px; */
+    /* border-radius: 20px; */
     background: #0faf87;
     color: white;
     text-align: center;
-    padding: 100px 0;
+    /* padding: 100px 0; */
     margin: 40px auto;
   }
 </style>
